ee228c7e5fe08320ac73b513cf0e04b8aa81cb4c
Raymond An <raymondan100@gmail.com>
2020-10-27 22:34:39 -0700

    Comment 3Sum Problem for clarity

--- src/ArrayProblems.java
+++ src/ArrayProblems.java
@@ -3,24 +3,32 @@ import java.util.*;
 
 public class ArrayProblems {
 
-
-    // {-2,-1,0,1,2,3}
     public List<List<Integer>> threeSum (int[] nums) {
 
+        // if nums array is less than 3, then return empty list []
         if (nums.length < 3) return new ArrayList<>();
 
+        // sort the nums array
         Arrays.sort(nums);
 
+        // use a set in order to achieve unique results
         Set<List<Integer>> uniqueresults = new HashSet<>();
 
         for (int i = 0; i < nums.length - 2; i++) {
+            // index next to i
             int j = i + 1;
+            // last index
             int k = nums.length - 1;
 
             while (j < k) {
                 int sum = nums[i] + nums[j] + nums[k];
+                // if the sum == 0, add it to the set
                 if (sum == 0) uniqueresults.add(Arrays.asList(nums[i], nums[j++], nums[k--]));
+                // else if sum > 0 that means you have to decrement k to get sum lower to
+                // potentially reach 0
                 else if (sum > 0) k--;
+                // else if sum < 0 that means you have to mve j to get sum higher to potentially
+                // reach zero
                 else j++;
             }
         }
