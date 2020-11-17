0adaae2cb597d61e80ea0e078f2c9f4ead7e8a0b
Raymond An <raymondan100@gmail.com>
2020-11-17 14:24:21 -0800

    Finish merge sorted array problem

--- src/ArrayProblems.java
+++ src/ArrayProblems.java
@@ -157,4 +157,24 @@ public class ArrayProblems {
         return result;
     }
 
+    // Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+
+    // Input:
+    // nums1 = [1,2,3,0,0,0], m = 3
+    // nums2 = [2,5,6],       n = 3
+
+    // Output: [1,2,2,3,5,6]
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        int j = 0;
+        for (int i = m; i < m + n; i++) {
+            nums1[i] = nums2[j];
+            j++;
+        }
+
+        Arrays.sort(nums1);
+
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,21 @@ public class Main {
 
     public static void main(String[] args) {
 
-        StringProblems stringProblems = new StringProblems();
-
-//        String[] strs = {"c","acc","ccc"};
-        int result = stringProblems.firstUniqChar("aadadaad");
-
-        System.out.println(result);
-
-//        ArrayProblems arrayProblems = new ArrayProblems();
+//        StringProblems stringProblems = new StringProblems();
 //
-//        int[] nums = {};
-//        int[] result = arrayProblems.searchRange(nums, 0);
+//        int result = stringProblems.firstUniqChar("aadadaad");
 //
-//        for (int num : result) {
-//            System.out.print(num + " ");
-//        }
+//        System.out.println(result);
+
+        ArrayProblems arrayProblems = new ArrayProblems();
+
+        int[] nums1 = {1,2,3,0,0,0};
+        int[] nums2 = {2,5,6};
+        arrayProblems.merge(nums1, 3, nums2,3);
+
+//        int[] nums1 = {0};
+//        int[] nums2 = {1};
+//        arrayProblems.merge(nums1, 0, nums2,1);
 
     }
 }
