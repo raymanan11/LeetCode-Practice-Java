d2b9adf273fc35d9930b75f47bce2d24cd808a7e
Raymond An <raymondan100@gmail.com>
2020-12-10 12:39:00 -0800

    Finish sum two numbers linkedlist problem

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-        StringProblems stringProblems = new StringProblems();
-        String s = "aA";
-        String result = stringProblems.reverseVowels(s);
-        System.out.println(result);
+//        StringProblems stringProblems = new StringProblems();
+//        String s = "aA" +
+//                "";
+//        String result = stringProblems.reverseVowels(s);
+//        System.out.println(result);
 
 //        ArrayProblems arrayProblems = new ArrayProblems();
 //        int[] nums = {0,0,1,1,1,1,2,3,3};
 //        int result = arrayProblems.removeDuplicates(nums);
 //        System.out.println(result);
+
     }
 }
