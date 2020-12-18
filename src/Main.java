55737bfb3fb41f85b25b996b90388d5164fa677a
Raymond An <raymondan100@gmail.com>
2020-12-18 15:41:02 -0800

    Finish Valid Palindrome II string problem

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
 
-//        StringProblems stringProblems = new StringProblems();
-//        String s = "aA" +
-//                "";
-//        String result = stringProblems.reverseVowels(s);
-//        System.out.println(result);
+        StringProblems stringProblems = new StringProblems();
+        boolean result = stringProblems.validPalindrome("qnpemmugscxpogaqatzjvmfssensnahzuazzkiguzppabkczxvgeevxzckbappzgikzzauzhansnessfmvjztaqagopxcsgummepnq");
+        System.out.println(result);
 
-        ArrayProblems arrayProblems = new ArrayProblems();
-        int[] nums = {2,4,9,3};
-        int[] result = arrayProblems.decrypt(nums, -2);
-        for (int num : result) {
-            System.out.print(num + " ");
-        }
+//        ArrayProblems arrayProblems = new ArrayProblems();
+//        int[] nums = {2,2,3,4};
+//        int result = arrayProblems.findLucky(nums);
+//        System.out.println(result);
 //        Misc misc = new Misc();
 //        int result = misc.divide(-2147483648, -1);
 //        System.out.println(result);
+
     }
 }
--- src/StringProblems.java
+++ src/StringProblems.java
@@ -358,6 +358,34 @@ public class StringProblems {
         }
         return result;
     }
+
+    // Given a non-empty string s, you may delete at most one character.
+    // Judge whether you can make it a palindrome.
+
+    // Input: "abca"
+    // Output: True
+    // Explanation: You could delete the character 'c'.
+
+    public boolean validPalindrome(String s) {
+        int middle = s.length() / 2;
+        for (int i = 0; i < middle; i++) {
+            if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
+                int j = s.length() - 1 - i;
+                return isPal(s, i, j - 1) || isPal(s, i + 1, j);
+            }
+        }
+        return true;
+    }
+
+    public boolean isPal(String s, int start, int end) {
+        while (start < end) {
+            if (s.charAt(start) != s.charAt(end)) return false;
+            start++;
+            end--;
+        }
+        return true;
+    }
+
 }
 
 
