781ea213f3a78bdc93279a675581322236600141
Raymond An <raymondan100@gmail.com>
2020-12-20 20:06:28 -0800

    Finish Max Repeating String Problem

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         StringProblems stringProblems = new StringProblems();
-        boolean result = stringProblems.isLongPressedName("pyplrz", "ppyypllr");
+        int result = stringProblems.maxRepeating("aaabaaaabaaabaaaabaaaabaaaabaaaaba", "aaaba");
         System.out.println(result);
 
 //        ArrayProblems arrayProblems = new ArrayProblems();
--- src/StringProblems.java
+++ src/StringProblems.java
@@ -557,6 +557,24 @@ public class StringProblems {
         return true;
     }
 
+    // For a string sequence, a string word is k-repeating if word concatenated k times is a substring of sequence.
+    // The word's maximum k-repeating value is the highest value k where word is k-repeating in sequence.
+    // If word is not a substring of sequence, word's maximum k-repeating value is 0.
+
+    // Given strings sequence and word, return the maximum k-repeating value of word in sequence.
+
+    public int maxRepeating(String sequence, String word) {
+        int repeating = 0;
+        String repeatingString = word;
+        while (true) {
+            if (sequence.contains(repeatingString)) {
+                repeating++;
+                repeatingString = repeatingString.concat(word);
+            }
+            else return repeating;
+        }
+    }
+
 }
 
 
