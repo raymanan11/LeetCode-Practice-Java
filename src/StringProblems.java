48765d813ca361a52eb5ecf8dd830d15cdca287b
Raymond An <raymondan100@gmail.com>
2020-12-26 22:00:19 -0800

    Finish Rotate Matrix CtCI

--- src/StringProblems.java
+++ src/StringProblems.java
@@ -792,6 +792,24 @@ public class StringProblems {
         return (compressedString.toString().length() < s.length()) ? compressedString.toString() : s;
     }
 
+    public void reverseMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i; j < matrix.length; j++) {
+                int temp = matrix[j][i];
+                matrix[j][i] = matrix[i][j];
+                matrix[i][j] = temp;
+            }
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length - 1 - j];
+                matrix[i][matrix.length - 1 - j] = temp;
+            }
+        }
+    }
+
 }
 
 
