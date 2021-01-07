4f34aaa75c8f7aaeb4d5b9248479c799986b5fdf
Raymond An <raymondan100@gmail.com>
2021-01-06 23:21:39 -0800

    Finish merge binary search trees

--- src/BST.java
+++ src/BST.java
@@ -20,6 +20,8 @@ public class BST {
         return sum;
     }
 
+    // Given the root node of a binary search tree, return the sum of values of all nodes with a value in the range [low, high].
+
     public void DFS(TreeNode node, int low, int high) {
         if (node != null) {
             if (node.val >= low && node.val <= high)
@@ -31,5 +33,20 @@ public class BST {
         }
     }
 
-    
+    // Given two binary trees and imagine that when you put one of them to cover the other,
+    // some nodes of the two trees are overlapped while the others are not.
+
+    // You need to merge them into a new binary tree. The merge rule is that if two nodes overlap,
+    // then sum node values up as the new value of the merged node.
+    // Otherwise, the NOT null node will be used as the node of new tree.
+
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null) return t2;
+        if (t2 == null) return t1;
+        t1.val += t2.val;
+        t1.left = mergeTrees(t1.left, t2.left);
+        t1.right = mergeTrees(t1.right, t2.right);
+        return t1;
+    }
+
 }
