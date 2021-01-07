9d3f07c1383df4e94544ff1bb362b84f11e3917e
Raymond An <raymondan100@gmail.com>
2021-01-06 23:51:15 -0800

    Finish Search BST problem

--- src/BST.java
+++ src/BST.java
@@ -49,4 +49,21 @@ public class BST {
         return t1;
     }
 
+    // Given the root node of a binary search tree (BST) and a value.
+    // You need to find the node in the BST that the node's value equals the given value.
+    // Return the subtree rooted with that node. If such node doesn't exist, you should return NULL.
+
+    TreeNode treeNode = null;
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) return null;
+        if (root.val == val) {
+            System.out.println("Found value: " + root.val);
+            treeNode = root;
+        }
+        System.out.println(root.val);
+        searchBST(root.left, val);
+        searchBST(root.right, val);
+        return treeNode;
+    }
+
 }
