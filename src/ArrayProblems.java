97aca4af30edd72250df97bfdb0b68ed079b9a2d
Raymond An <raymondan100@gmail.com>
2021-01-18 18:20:08 -0800

    Redo Merge Two Sorted Arrays

--- src/ArrayProblems.java
+++ src/ArrayProblems.java
@@ -150,6 +150,10 @@ public class ArrayProblems {
 
     // Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
 
+    // The number of elements initialized in nums1 and nums2 are m and n respectively.
+    // You may assume that nums1 has a size equal to m + n such that it has enough space
+    // to hold additional elements from nums2.
+
     // Input:
     // nums1 = [1,2,3,0,0,0], m = 3
     // nums2 = [2,5,6],       n = 3
@@ -157,15 +161,12 @@ public class ArrayProblems {
     // Output: [1,2,2,3,5,6]
 
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-
         int j = 0;
         for (int i = m; i < m + n; i++) {
             nums1[i] = nums2[j];
             j++;
         }
-
         Arrays.sort(nums1);
-
     }
 
     // Given an array nums and a value val,
